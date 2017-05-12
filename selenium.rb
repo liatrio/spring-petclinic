998bda236b861e5f2760a1867ce8bf88f349cf23
Benjamin Stein <ben@liatrio.com>
2017-05-12 09:29:51 -0700

    adding first test

--- selenium.rb
+++ selenium.rb
@@ -8,4 +8,8 @@ headless.start
 driver = Selenium::WebDriver.for :firefox
 driver.navigate.to 'http://dashboard:8080/petclinic'
 puts "Page title: #{driver.title}"
+
+home_button = driver.find_elements(:class_name, "icon-home")
+puts "Element home:  #{home_button.text}"
+
 headless.destroy
