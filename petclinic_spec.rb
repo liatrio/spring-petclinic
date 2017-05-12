e09d8a0b644200f1f97d9f36ff9b0d1c4c4b991d
Benjamin Stein <ben@liatrio.com>
2017-05-12 14:01:08 -0700

    removing dockerfile

--- petclinic_spec.rb
+++ petclinic_spec.rb
@@ -10,6 +10,7 @@ describe 'Petlinic' do
 
     @driver = Selenium::WebDriver.for :firefox
     @driver.navigate.to 'http://dashboard:8080/petclinic'
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 3)
   end
 
   after do
@@ -23,10 +24,14 @@ describe 'Petlinic' do
     end
   end
 
-  # describe 'when homepage is available' do
-  #   it "I should see Home button" do
-  #     home_button = driver.find_elements(:class_name, 'icon-home')
-  #     puts "Element home:  #{home_button.text}"
-  #   end
-  # end
+
+  describe 'when homepage is available' do
+    it 'click the find owners button' do
+        @driver.find_element(:class, "icon-search").click
+        form = @driver.find_element(:id, "lastName")
+        puts "form is  #{form.label}"
+      assert @driver.title == "PetClinic :: a Spring Framework demonstration"
+    end
+  end
+
 end
